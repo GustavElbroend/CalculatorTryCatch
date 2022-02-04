5ec01be4d6d37d754daec586bfd2ec710d05def6
Gustav Joachim Elbrønd <202006022@post.au.dk>
2022-02-04 08:59:21 +0100

    Add project files.

---
+++ CalculatorTryCatch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
