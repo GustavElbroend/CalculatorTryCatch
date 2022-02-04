42de55293c29af5f83d1c964cf0018e1f7b27d49
Gustav Joachim Elbrønd <202006022@post.au.dk>
2022-02-04 09:35:57 +0100

    Working calculator, but no NUnitTest or Trycatch

---
+++ CalculatorTryCatch/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTryCatch
+{
+    public class Calculator
+    {
+        //This is going to be a simple calculator, that does not handles large numbers
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Power(double a, double b)
+        {
+            return Math.Pow(a,b);
+        }
+    }
+}
--- CalculatorTryCatch/Program.cs
+++ CalculatorTryCatch/Program.cs
@@ -6,7 +6,58 @@ namespace CalculatorTryCatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool terminate = true;
+            while (terminate)
+            {
+
+                Calculator calculator = new Calculator();
+                Console.WriteLine("Please select type of calculation");
+                Console.WriteLine("1=Add, 2=Subtract, 3=Multiply, 4=Power");
+
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                Console.WriteLine("Type the two numbers:");
+                double a = Convert.ToDouble(Console.ReadLine());
+                double b = Convert.ToDouble(Console.ReadLine());
+                double result = 0;
+                string operation = "";
+
+                switch (key.KeyChar)
+                {
+                    case '1':
+                        result = calculator.Add(a, b);
+                        operation = "Add";
+                        break;
+                    case '2':
+                        result = calculator.Subtract(a, b);
+                        operation = "Subtract";
+                        break;
+                    case '3':
+                        result = calculator.Multiply(a, b);
+                        operation = "Multiply";
+                        break;
+                    case '4':
+                        result = calculator.Power(a, b);
+                        operation = "Power";
+                        break;
+                }
+
+                Console.WriteLine($"You chose {operation}, and typed: {a}, {b}.\nThe result is: {result}");
+
+                Console.WriteLine("\nDo you want to do another operation?");
+                var choice = Console.ReadKey(true);
+                switch (choice.KeyChar)
+                {
+                    case 'y':
+                    case 'Y':
+                        terminate = true;
+                        break;
+                    case 'n':
+                    case 'N':
+                        terminate = false;
+                        break;
+                }
+            }
         }
     }
 }
